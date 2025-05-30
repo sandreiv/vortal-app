c21bf6805f12781fe61e1aeabd9d98682bff2a61
Daniel Arias <dajo172020@gmail.com>
2025-05-30 11:31:00 -0500

    Modificaciones en dashboard para integración de Gridster

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,14 +1,45 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // <-- importa esto
+import { GridsterConfig, GridsterItem, GridsterModule } from 'angular-gridster2';
+
 import { StatsWidgetComponent } from './components/statswidget';
 import { RecentSalesWidgetComponent } from './components/recentsaleswidget';
 import { BestSellingWidgetComponent } from './components/bestsellingwidget';
 import { RevenueStreamWidgetComponent } from './components/revenuestreamwidget';
 import { NotificationsWidgetComponent } from './components/notificationswidget';
 
+
+interface WidgetItem extends GridsterItem {
+  component: 'stats' | 'sales' | 'best' | 'revenue' | 'notifications';
+}
+
+
+
 @Component({
     selector: 'app-dashboard',
     standalone: true,
-    imports: [StatsWidgetComponent, RecentSalesWidgetComponent, BestSellingWidgetComponent, RevenueStreamWidgetComponent, NotificationsWidgetComponent],
+    imports: [CommonModule,GridsterModule,StatsWidgetComponent, RecentSalesWidgetComponent, BestSellingWidgetComponent, RevenueStreamWidgetComponent, NotificationsWidgetComponent],
     templateUrl: './dashboard.component.html'
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  options: GridsterConfig = {
+  draggable: { enabled: true },
+  resizable: { enabled: true },
+  pushItems: true,
+  displayGrid: 'onDrag&Resize',
+  gridType: 'fixed',
+  fixedColWidth: 120,
+  fixedRowHeight: 100,
+  margin: 10
+    };
+
+
+  dashboard: WidgetItem[] = [
+  { cols: 4, rows: 2, y: 0, x: 0, component: 'stats' },
+  { cols: 6, rows: 2, y: 0, x: 4, component: 'sales' },
+  { cols: 6, rows: 2, y: 2, x: 4, component: 'best' },
+  { cols: 6, rows: 2, y: 2, x: 0, component: 'revenue' },
+  { cols: 6, rows: 2, y: 4, x: 0, component: 'notifications' }
+    ];
+
+}
